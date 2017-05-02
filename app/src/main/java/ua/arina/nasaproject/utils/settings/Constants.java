4fc7da5395abc412a380daf08e61013874ce15f7
HitStalker-GH <hitstalker7@gmail.com>
2017-05-02 17:11:12 -0400

    Updated Apache 2.0 license

--- app/src/main/java/ua/arina/nasaproject/utils/settings/Constants.java
+++ app/src/main/java/ua/arina/nasaproject/utils/settings/Constants.java
@@ -7,3 +7,19 @@ package ua.arina.nasaproject.utils.settings;
 public final class Constants {
     public static String BASE_URL = "http://environment.data.gov.uk/";
 }
+/**
+ * Copyright 2017 Oryna Starkina
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    ttp://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
